c761441eaf96ce34cc6a3f37382cacb3790358c4
zmemon <zainmemon010@gmail.com>
2023-10-24 09:18:33 +0500

    Update card-item.tsx

--- src/components/card-item.tsx
+++ src/components/card-item.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent } from "react"
-import { CodeSandboxCircleFilled, ConsoleSqlOutlined, EditOutlined, PlusOutlined, SaveOutlined } from "@ant-design/icons";
+import { CodeSandboxCircleFilled, ConsoleSqlOutlined, DeleteOutlined, EditOutlined, PlusOutlined, SaveOutlined } from "@ant-design/icons";
 import { Card, Checkbox, List, Button, Input } from "antd"
 
 export type Item = {
@@ -11,31 +11,32 @@ type ComponentProps = {
     title: string;
     onClick: (checked: boolean, idx: number) => void;
     addItem: (e: string) => void;
-    updateItem : (value: string, idx: number) => void
+    updateItem: (value: string, idx: number) => void;
+    deleteItem: (idx:number) => void
 }
 
 type InputChange = {
-    [key : number] : string
+    [key: number]: string
 }
 
-const CardItem: React.FC<ComponentProps> = ({ items, title, onClick, addItem, updateItem }): JSX.Element => {
+const CardItem: React.FC<ComponentProps> = ({ items, title, onClick, addItem, updateItem, deleteItem }): JSX.Element => {
     const [showInputBox, setShowInput] = React.useState([])
     const [inputChanges, setInputChanges] = React.useState<InputChange>({})
     const Title = (
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             {title}
-            <Button icon={<PlusOutlined />} onClick={() => {addItem('Item')}}></Button>
+            <Button icon={<PlusOutlined />} onClick={() => { addItem('Item') }}></Button>
         </div>
     )
     const handleSave = (idx: number) => {
         setShowInput(showInputBox.filter((i) => i != idx))
-        if(inputChanges[idx]){
+        if (inputChanges[idx]) {
             updateItem(inputChanges[idx], idx)
-            setInputChanges(Object.keys(inputChanges).filter((index) => Number(index) != idx ?? {[index] : inputChanges[Number(index)]} ))        
+            setInputChanges(Object.keys(inputChanges).filter((index) => Number(index) != idx ?? { [index]: inputChanges[Number(index)] }))
         }
     }
     const handleChange = (value: string, idx: number) => {
-        setInputChanges({...inputChanges, [idx]: value})
+        setInputChanges({ ...inputChanges, [idx]: value })
     }
     return (
         <Card title={Title} hoverable style={{ width: '40%' }}>
@@ -53,7 +54,10 @@ const CardItem: React.FC<ComponentProps> = ({ items, title, onClick, addItem, up
                                 ) : (
                                     <>
                                         <Checkbox checked={item.checked} onClick={(e: React.MouseEvent<HTMLInputElement>) => onClick(e.currentTarget.checked, idx)}>{item.item}</Checkbox>
-                                        <Button style={{ marginLeft: 10 }} icon={<EditOutlined />} onClick={() => setShowInput([...showInputBox, idx])}></Button>
+                                        <div>
+                                            <Button style={{ marginLeft: 10 }} icon={<EditOutlined />} onClick={() => setShowInput([...showInputBox, idx])}></Button>
+                                            <Button style={{ marginLeft: 10 }} icon={<DeleteOutlined />} onClick={() => deleteItem(idx)}></Button>
+                                        </div>
                                     </>
                                 )
                         }
